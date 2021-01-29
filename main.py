2a45342cebb25a21021a2b0972e677beaada48ad
Liken99 <47985395+Liken99@users.noreply.github.com>
2021-01-29 14:17:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,125 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from __future__ import annotations
+from typing import List
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Ebay:
+    def attach(self, buyer): pass
+    def detach(self, buyer): pass
+    def notify(self): pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Subject(Ebay):
+    status: List
+    buyer: List[Buyer] = []
+    def attach(self, observer):
+        print("Ebay: Добавился подписчик.")
+        self.buyer.append(observer)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def detach(self, observer):
+        self.buyer.remove(observer)
+
+    def notify(self):
+        print("Ebay: Идет оповещение...")
+        for observer in self.buyer:
+            observer.update(self)
+
+    def action(self, _status):
+        print("\nEbay: Поступление товара.")
+        self.status = _status
+
+        print(f"Ebay: В наличии появилось: {self.status}")
+        self.notify()
+
+class Buyer:
+    def update(self, _subject): pass
+
+class Buyer_a(Buyer):
+    _name: str
+    name: str
+    c: List = []
+    f: List = []
+
+    def prod(self, _name):
+        self.name = _name
+
+    def update(self, _subject):
+
+        for a in self.name:
+            for b in _subject.status:
+                if a == b:
+                    self.c.append(a)
+
+        if self.c == self.name:
+            print("Buyer_a: Reacted")
+        else:
+            self.f = list(set(self.name) - set(self.c))
+
+            print("Buyer_a: Данного продукта нет в наличии: ", self.f)
+
+class Buyer_b(Buyer):
+    _name: str
+    name: str
+    c: List = []
+    f: List = []
+
+    def prod(self, _name):
+        self.name = _name
+
+    def update(self, _subject):
+
+        for a in self.name:
+            for b in _subject.status:
+                if a == b:
+                    self.c.append(a)
+
+        if self.c == self.name:
+            print("Buyer_b: Reacted")
+        else:
+            self.f = list(set(self.name) - set(self.c))
+
+            print("Buyer_b: Данного продукта нет в наличии: ", self.f)
+
+class Buyer_c(Buyer):
+    _name: str
+    name: str
+    c: List = []
+    f: List = []
+    def prod(self, _name):
+        self.name = _name
+    def update(self, _subject):
+
+        for a in self.name:
+            for b in _subject.status:
+                if a == b:
+                    self.c.append(a)
+
+        if self.c == self.name:
+            print("Buyer_c: Reacted")
+        else:
+            self.f = list(set(self.name) - set(self.c))
+
+            print("Buyer_c: Данного продукта нет в наличии: ", self.f)
+
+
+
+
+
+subject = Subject()
+
+sub_a = Buyer_a()
+a = ["Kartoshka", "Rise"]
+sub_a.prod(a)
+subject.attach(sub_a)
+
+sub_b = Buyer_b()
+b = ["Moloko", "Pomidor"]
+sub_b.prod(b)
+subject.attach(sub_b)
+
+sub_c = Buyer_c()
+c = ["Voda", "Pirog"]
+sub_c.prod(c)
+subject.attach(sub_c)
+
+
+subject.action(["Voda", "Pirog","Moloko", "Kartoshka", "Rise", "Tea", "Spoon"])
